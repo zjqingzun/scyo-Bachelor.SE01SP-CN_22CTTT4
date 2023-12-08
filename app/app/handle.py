758342dd8e050e5bc957532eee385e490831d102
Sakate Kataro <120289465+SakateKataro@users.noreply.github.com>
2023-12-08 11:29:50 +0700

    Update handle.py

--- app/app/handle.py
+++ app/app/handle.py
@@ -28,19 +28,6 @@ def send_to_server(sock: socket, s: str) :
             raise ConnectionError
 
 
-def get_current_datetime():
-    current_datetime = datetime.now()
-    formatted_datetime = current_datetime.strftime("%Y%m%d%H%M%S")
-    return formatted_datetime
-
-
-
-
-    
-
-
-
-
 
 
 
